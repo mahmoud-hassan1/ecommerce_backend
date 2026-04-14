c3a4fd7e44c187e0c5451a6fac7bfd1cba1e0374
ziad <zeyadkabeel99@gmail.com>
2026-04-15 00:38:33 +0200

    add cat to index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import userRoutes from "./auth.js";
 import orderRoutes from "./orderRoutes.js";
 import ProductRoutes from "./ProductRoutes.js"
+import categoryRoutes from "./categoryRouts.js";
 const router = express.Router();
 router.use("/auth", userRoutes);
 router.use("/products",ProductRoutes);
 router.use("/orders", orderRoutes);
+router.use("/categories", categoryRoutes);
 export default router;
